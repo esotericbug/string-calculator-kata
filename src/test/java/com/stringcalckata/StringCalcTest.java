edf17464f17c7e9dd5b759f8c7e9738485da2dc9
Aakash Singh <aakashsingh93.as@gmail.com>
2024-11-18 17:23:44 +0530

    Added test to check for empty string

--- src/test/java/com/stringcalckata/StringCalcTest.java
+++ src/test/java/com/stringcalckata/StringCalcTest.java
@@ -1,7 +1,11 @@
 package com.stringcalckata;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class StringCalcTest {
 
@@ -15,6 +19,12 @@ public class StringCalcTest {
     @AfterEach
     public void destroy() {
         stringCalc = null;
+    }
+
+    @Test
+    @DisplayName("Test Empty String")
+    public void testEmptyString() {
+        assertEquals(0, stringCalc.add(""));
     }
 
 }
