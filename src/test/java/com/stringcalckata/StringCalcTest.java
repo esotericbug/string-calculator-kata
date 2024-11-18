ddafe89a3fcd18b9c6fb83f12a1482bbbc80fe5d
Aakash Singh <aakashsingh93.as@gmail.com>
2024-11-18 17:22:23 +0530

    Intial methods of StringCalcTest class

--- src/test/java/com/stringcalckata/StringCalcTest.java
+++ src/test/java/com/stringcalckata/StringCalcTest.java
@@ -1,5 +1,20 @@
 package com.stringcalckata;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 public class StringCalcTest {
-    
+
+    StringCalc stringCalc;
+
+    @BeforeEach
+    public void init() {
+        stringCalc = new StringCalc();
+    }
+
+    @AfterEach
+    public void destroy() {
+        stringCalc = null;
+    }
+
 }
